e9d07dad6aa90c6a650645fbc3f10752d8bfc5bc
victusfate <messel@gmail.com>
2021-12-07 16:09:13 -0500

    W2-3036 corrected queue creation, commented out training listener atm

--- bin/create_queue.py
+++ bin/create_queue.py
@@ -5,17 +5,17 @@ from recommend import constants
 from bandolier import message_queue
 
 # Create a SQS queue
-# mq = message_queue.MessageQueue(name=constants.TRAINING_QUEUE_ROOT_NAME,
-#   env=constants.ENVIRONMENT,
-#   region_name=constants.AWS_REGION,
-#   profile_name=constants.AWS_PROFILE)
-# mq.create_queue()
+mq = message_queue.MessageQueue(name=constants.TRAINING_QUEUE_ROOT_NAME,
+  env=constants.ENVIRONMENT,
+  region_name=constants.AWS_REGION,
+  profile_name=constants.AWS_PROFILE)
+mq.create_queue()
 
-# mq = message_queue.MessageQueue(name=constants.TRAINING_QUEUE_ROOT_NAME,
-#   env='d2',
-#   region_name=constants.AWS_REGION,
-#   profile_name=constants.AWS_PROFILE)
-# mq.create_queue()
+mq = message_queue.MessageQueue(name=constants.TRAINING_QUEUE_ROOT_NAME,
+  env='d2',
+  region_name=constants.AWS_REGION,
+  profile_name=constants.AWS_PROFILE)
+mq.create_queue()
 
 mq = message_queue.MessageQueue(name=constants.TRAINING_QUEUE_ROOT_NAME,
   env='beta',
@@ -23,8 +23,8 @@ mq = message_queue.MessageQueue(name=constants.TRAINING_QUEUE_ROOT_NAME,
   profile_name=constants.AWS_PROFILE)
 mq.create_queue()
 
-# mq = message_queue.MessageQueue(name=constants.TRAINING_QUEUE_ROOT_NAME,
-#   env='p2',
-#   region_name=constants.AWS_REGION,
-#   profile_name=constants.AWS_PROFILE)
-# mq.create_queue()
+mq = message_queue.MessageQueue(name=constants.TRAINING_QUEUE_ROOT_NAME,
+  env='prod',
+  region_name=constants.AWS_REGION,
+  profile_name=constants.AWS_PROFILE)
+mq.create_queue()
