64e75326a642b113ca30d66881e83bdf563dd9f0
victusfate <messel@gmail.com>
2021-12-07 16:16:54 -0500

    W2-3036 couple of corrections, still hitting an issue creating the queue

--- bin/create_queue.py
+++ bin/create_queue.py
@@ -1,8 +1,15 @@
 import boto3
 import sys
 from sys import platform
-from recommend import constants
+from concierge import constants
 from bandolier import message_queue
+
+print({
+  'training queue': constants.TRAINING_QUEUE_ROOT_NAME,
+  'env': constants.ENVIRONMENT,
+  'region_name': constants.AWS_REGION,
+  'profile_name': constants.AWS_PROFILE
+})
 
 # Create a SQS queue
 mq = message_queue.MessageQueue(name=constants.TRAINING_QUEUE_ROOT_NAME,
