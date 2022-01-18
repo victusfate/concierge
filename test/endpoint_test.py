3f083a816a62b090fdb13cdf8433d4cb7ebe891e
victusfate <messel@gmail.com>
2022-01-18 13:11:24 -0500

    W2-3254 nailed it'

--- test/endpoint_test.py
+++ test/endpoint_test.py
@@ -13,6 +13,6 @@ tModelEnd = time.time()
 item_ids = cf.random_items(30)
 user_id = '128x9v1'
 rget = requests.get('http://localhost:5000/user/' + user_id + '/items/' + ','.join(item_ids))
-print(rget.json())
+print('get',rget.json())
 rpost = requests.post('http://localhost:5000/user/' + user_id + '/items',json={'items': item_ids})
-print(rpost.json())
+print('post',rpost.json())
