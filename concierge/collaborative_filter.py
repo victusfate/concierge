8ef8283550717bbca5b0e3056aff741c5a0aa7be
victusfate <messel@gmail.com>
2022-03-15 17:02:15 -0400

    W2-3402 warning not warn hmm

--- concierge/collaborative_filter.py
+++ concierge/collaborative_filter.py
@@ -61,7 +61,7 @@ class CollaborativeFilter:
     self.metric = pickle.load(open(metric_path,'rb'))
     # ensure we always have a valid timestamp for delta training
     if self.model.timestamp is None:
-      log.warn('load_from_file',self.name,'model.timestamp is None, setting to now')
+      log.warning('load_from_file',self.name,'model.timestamp is None, setting to now')
       self.model.timestamp = time.time()
     
 
